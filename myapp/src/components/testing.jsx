d0f56acc6cadda8e1d926ab3fc9c39c5b350c017
DevCodeYuvraj <yuvrajsingh27052002@gmail.com>
2026-05-22 17:03:41 +0000

    A30110:33 PM142 C14233i10:33 PM

--- myapp/src/components/testing.jsx
+++ myapp/src/components/testing.jsx
@@ -26,7 +26,7 @@ const increment5 = () =>{
   <Card className='cardstyle'>
                     <Card.Title className='cardtitle'><h2>Bootstrap dashboard application</h2> </Card.Title>
                     <Card.Text className='cardtext'>
-                        <h5 className='previousprice align-start'  >Web Development</h5></Card.Text>
+                        <h5 className='previousprice d-flex start'  >Web Development</h5></Card.Text>
                     <Card.Body className='cardbody'>
 
                         
