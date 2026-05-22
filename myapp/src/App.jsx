6a7f106643089e677a87ac4ecdc8d8d78877a548
DevCodeYuvraj <yuvrajsingh27052002@gmail.com>
2026-05-22 16:47:18 +0000

    A33610:17 PM142 C14217i10:17 PM

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Routing from "./Routing/routing";
-// import BasicExample from "./components/testing";
+// import Routing from "./Routing/routing";
+import AnimatedExample from "./components/testing";
 function App() {
   return (
-    <Routing />
-    // <BasicExample/>
+    // <Routing />
+    <BasicExample/>
 
   )
 }
