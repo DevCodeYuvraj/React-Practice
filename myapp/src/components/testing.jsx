77f0aeca0140f8b96ab4b451f0b880e3cb0c1773
DevCodeYuvraj <yuvrajsingh27052002@gmail.com>
2026-05-22 16:50:58 +0000

    A33810:20 PM142 C14220i10:20 PM

--- myapp/src/components/testing.jsx
+++ myapp/src/components/testing.jsx
@@ -1,7 +1,6 @@
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import {useState} from 'react';
 import { Card } from 'react-bootstrap';
-
 function AnimatedExample() {
 const [AddProgress,setAddProgress]=useState(5);
 const increment5 = () =>{
