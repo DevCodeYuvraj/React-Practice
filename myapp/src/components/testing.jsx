9b0bbca077fb5bfa4ebba039d000c203fbe70d74
DevCodeYuvraj <yuvrajsingh27052002@gmail.com>
2026-05-22 16:49:34 +0000

    A80410:19 PM142 C14219i10:19 PM

--- myapp/src/components/testing.jsx
+++ myapp/src/components/testing.jsx
@@ -20,6 +20,26 @@ const increment5 = () =>{
   <ProgressBar className='bar1' animated now={AddProgress} label={`${AddProgress}%`} /> 
   <br />
   <ProgressBar animated variant="danger"  now={AddProgress} />
+
+  <Card className='cardstyle'>
+                    <Card.Title className='cardtitle'>Bluetooth Headphones</Card.Title>
+                    <Card.Img className='cardimg' variant="top" src={cardimage1} />
+                    <Card.Body className='cardbody'>
+
+                        <Card.Text className='cardtext'>
+                            Boult Newly Launched Flex On Ear Bluetooth Headphones with 80H Playtime, 40mm Bass Drivers, Zen™ ENC Mic, Type-C Fast Charging, Bluetooth 5.4, AUX Option, 60ms Low Latency, IPX5 (Jet Black)
+                        </Card.Text>
+                        <Stack className="price" direction='horizontal' gap={2}>₹1,399 <div className="previousprice" ><s>MRP: ₹3,999</s></div></Stack>
+                        <hr />
+                        <div className='buttonbackground'>
+                            <Stack direction="horizontal" >
+
+                                <Button className="cartbutton" >Add To Cart</Button>
+                                <Button className="buybutton" variant="primary">Buy Now</Button>
+                            </Stack>
+                        </div>
+                    </Card.Body>
+                </Card>
   </>
   ) 
 };
