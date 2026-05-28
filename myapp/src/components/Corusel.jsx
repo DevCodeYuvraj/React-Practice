c1a593112ce200798d5b0f724b578c4445af4dd7
DevCodeYuvraj <yuvrajsingh27052002@gmail.com>
2026-05-28 11:42:20 +0000

    A3115:12 PM148 C14812i5:12 PM

--- myapp/src/components/Corusel.jsx
+++ myapp/src/components/Corusel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Carousel, Container } from 'react-bootstrap';
-import Banner1 from '../assets/hongkong1081704.png';
+import Banner1 from '../assets/hongkong1081704.png'
 const RBCarousel = () => {
   // Replace these placeholders with your actual downloaded banner URLs
   const banners = [
